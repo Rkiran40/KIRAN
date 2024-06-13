226cd42364fb41f8dbca9424520c1b60b44c9239
Rkiran40 <rkirangh07@gmail.com>
2024-06-13 16:02:38 +0530

    Add files via upload

---
+++ rk1.java
@@ -0,0 +1,16 @@
+import java.co.*;
+import java.until.Scanner;
+class studnt
+{
+public static void main(String args[])
+{
+Scanner sc= new Scanner (System.in);
+System.out.print ln("enter student name");
+String name=Sc.next Int();
+System.uot.print in("enter student rno");
+int rno= Sc.next int();
+System.out.print in("student name is"+name);
+System.out.print in ("student rno.is"+rno);
+}
+}
+
